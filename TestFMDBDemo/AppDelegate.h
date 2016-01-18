46e2dbac3eb738897947a16cebdcb7de9c64e946
XIAOLONG.YU <weirdyu@foxmail.com>
2016-01-18 13:54:39 +0800

    Initial Commit

---
+++ TestFMDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFMDBDemo
+//
+//  Created by Mac on 16/1/18.
+//  Copyright © 2016年 Beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
